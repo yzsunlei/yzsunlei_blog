e59027f4c84e5f5cdefb1c294b2b2a09ca938548
sunlei <sunlei@jxgtmbjt.com>
2023-02-08 21:24:42 +0800

    feat: bigfed_notes子项目添加

---
+++ packages/bigfed_notes/docs/.vuepress/config.js
@@ -0,0 +1,65 @@
+module.exports = {
+  base: "/bigfed_notes/",
+  title: "小磊哥er的大前端学习笔记",
+  themeConfig: {
+    nav: [
+      {
+        text: "首页",
+        link: "/",
+      },
+      {
+        text: "基础系列",
+        items: [
+          { text: 'A.《响应式Web设计:HTML5和CSS3实战》', link: '/bigfed_notes/01.basis/A.《响应式Web设计:HTML5和CSS3实战》.html', target:'_blank' },
+          { text: 'B.《CSS世界》', link: '/bigfed_notes/01.basis/B.《CSS世界》.html', target:'_blank' },
+          { text: 'C.《学习JavaScript数据结构与算法-第2版》', link: '/bigfed_notes/01.basis/C.《学习JavaScript数据结构与算法-第2版》.html', target:'_blank' },
+          { text: 'D.《基于MVC的JavaScript_Web富应用开发》', link: '/bigfed_notes/01.basis/D.《移动WEB前端高级开发实践》.html', target:'_blank' },
+          { text: 'E.《高性能网站建设指南》', link: '/bigfed_notes/01.basis/E.《高性能网站建设指南》.html', target:'_blank'},
+          { text: 'E.《你不知道的JavaScript（上卷）》', link: '/bigfed_notes/01.basis/E.《你不知道的JavaScript（上卷）》.html', target:'_blank'},
+          { text: 'E.《你不知道的JavaScript（下卷）》', link: '/bigfed_notes/01.basis/E.《你不知道的JavaScript（下卷）》.html', target:'_blank'},
+          { text: 'E.《图解HTTP》', link: '/bigfed_notes/01.basis/E.《图解HTTP》.html', target:'_blank'},
+          { text: 'E.《JavaScript_ES6函数式编程入门经典》', link: '/bigfed_notes/01.basis/E.《JavaScript_ES6函数式编程入门经典》.html', target:'_blank'}
+        ],
+      },
+      {
+        text: "进阶系列",
+        items: [
+          { text: 'E.《JavaScript设计模式与开发实践》', link: '/bigfed_notes/02.advance/E.《JavaScript设计模式与开发实践》.html', target:'_blank' },
+          { text: 'E.《JavaScript异步编程》', link: '/bigfed_notes/02.advance/E.《JavaScript异步编程》.html', target:'_blank' },
+          { text: 'G.《React进阶之路》', link: '/bigfed_notes/02.advance/G.《React进阶之路》.html', target:'_blank' },
+          { text: 'H.《前端工程化：体系设计与实践》', link: '/bigfed_notes/02.advance/H.《前端工程化：体系设计与实践》.html', target:'_blank' },
+          { text: 'H.《深入浅出Webpack》', link: '/bigfed_notes/02.advance/H.《深入浅出Webpack》.html', target:'_blank' },
+          { text: 'J.《React_Native精解与实战》', link: '/bigfed_notes/02.advance/J.《React_Native精解与实战》.html', target:'_blank' },
+          { text: 'J.《React_Native移动开发实战》', link: '/bigfed_notes/02.advance/J.《React_Native移动开发实战》.html', target:'_blank' },
+          { text: 'K.《深入浅出Node.js》', link: '/bigfed_notes/02.advance/K.《深入浅出Node.js》.html', target:'_blank' },
+          { text: 'K.《Koa与Node.js开发实战》', link: '/bigfed_notes/02.a d/K.《Koa与Node.js开发实战》.html', target:'_blank' },
+        ],
+      },
+      {
+        text: "资深系列",
+        items: [
+          { text: 'L.《深入浅出Serverless-技术原理与应用实践》', link: '/bigfed_notes/03.senior/《深入浅出Serverless-技术原理与应用实践》.html', target:'_blank' },
+          { text: 'L.《算法图解》', link: '/bigfed_notes/03.senior/L.《算法图解》.html', target:'_blank'},
+          { text: 'L.《图解TCP_IP_第5版》', link: '/bigfed_notes/03.senior/L.《图解TCP_IP_第5版》.html', target:'_blank'},
+          { text: 'L.《微服务设计》', link: '/bigfed_notes/03.senior/L.《微服务设计》.html', target:'_blank'},
+          { text: 'L.《现代前端技术解析》', link: '/bigfed_notes/03.senior/L.《现代前端技术解析》.html', target:'_blank'},
+          { text: 'L.《云原生：运用容器、函数计算和数据构建下一代应用》', link: '/bigfed_notes/03.senior/L.《云原生：运用容器、函数计算和数据构建下一代应用》.html', target:'_blank'},
+          { text: 'L.《云原生架构进阶实战》', link: '/bigfed_notes/03.senior/L.《云原生架构进阶实战》.html', target:'_blank'},
+          { text: 'L.《DevOps和自动化运维实践》', link: '/bigfed_notes/03.senior/L.《DevOps和自动化运维实践》.html', target:'_blank'},
+          { text: 'L.《Docker技术入门与实战-第3版》', link: '/bigfed_notes/03.senior/L.《Docker技术入门与实战-第3版》.html', target:'_blank'},
+          { text: 'L.《Electron实战：入门、进阶与性能优化》', link: '/bigfed_notes/03.senior/L.《Electron实战：入门、进阶与性能优化》.html', target:'_blank'},
+          { text: 'L.《Serverless架构：从原理、设计到项目实战》_刘宇', link: '/bigfed_notes/03.senior/L.《Serverless架构：从原理、设计到项目实战》_刘宇.html', target:'_blank'},
+          { text: 'L.《Web前端黑客技术揭秘》', link: '/bigfed_notes/03.senior/L.《Web前端黑客技术揭秘》.html', target:'_blank'},
+        ]
+      },
+      {
+        text: "个人总结系列",
+        items: [
+
+        ]
+      }
+    ],
+    sidebar: 'auto',
+    sidebarDepth: 1
+  },
+};
