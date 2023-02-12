537af35db69686ed147ce3ed02dc3ae7324de419
sunlei <sunlei@jxgtmbjt.com>
2023-02-12 15:23:57 +0800

    feat: bigdata_notes子模块更新

--- packages/bigdata_notes/docs/.vuepress/config.js
+++ packages/bigdata_notes/docs/.vuepress/config.js
@@ -11,15 +11,25 @@ module.exports = {
         text: "基础系列",
         items: [
           { text: 'A.《图解算法-使用Python》', link: '/bigdata_notes/01.basis/A.《图解算法-使用Python》_吴灿铭等_201810.html', target:'_blank' },
+          { text: 'A.《Python基础教程（第3版）》待整理', link: '/bigdata_notes/01.basis/404.html', target:'_blank' },
+          { text: 'A.《Python核心编程（第3版）》待整理', link: '/bigdata_notes/01.basis/404.html', target:'_blank' },
+          { text: 'A.《Python高级编程.（法）莱德》待整理', link: '/bigdata_notes/01.basis/404.html', target:'_blank' },
+          { text: 'B.《Flask Web开发：基于Python的Web应用开发实战》待整理', link: '/bigdata_notes/01.basis/404.html', target:'_blank' },
+          { text: 'B.《深入理解Flask_（美）Jack Stouffer著》待整理', link: '/bigdata_notes/01.basis/404.html', target:'_blank' },
+          { text: 'B.《Python高效开发实战+Django+Tornado+Flask+Twisted》待整理', link: '/bigdata_notes/01.basis/404.html', target:'_blank' },
           { text: 'C.《精通Scrapy网络爬虫》', link: '/bigdata_notes/01.basis/C.《精通Scrapy网络爬虫》.html', target:'_blank' },
           { text: 'C.《Python3反爬虫原理与绕过实战》', link: '/bigdata_notes/01.basis/C.《Python3反爬虫原理与绕过实战》.html', target:'_blank' },
           { text: 'C.《Python3网络爬虫开发实战》', link: '/bigdata_notes/01.basis/C.《Python3网络爬虫开发实战》_崔庆才_201804.html', target:'_blank' },
           { text: 'C.《Python爬虫技术-深入理解原理、技术与开发》', link: '/bigdata_notes/01.basis/C.《Python爬虫技术-深入理解原理、技术与开发》.html', target:'_blank' },
+          { text: 'C.《精通Python网络爬虫 核心技术、框架与项目实战 ,韦玮》待整理', link: '/bigdata_notes/01.basis/404.html', target:'_blank' },
         ],
       },
       {
         text: "进阶系列",
         items: [
+          { text: 'D.《Python数据处理》待整理', link: '/bigdata_notes/02.advance/404.html', target:'_blank' },
+          { text: 'D.《Neo4j权威指南》待整理', link: '/bigdata_notes/02.advance/404.html', target:'_blank' },
+          { text: 'D.《Python数据分析与挖掘实战》待整理', link: '/bigdata_notes/02.advance/404.html', target:'_blank' },
           { text: 'D.《大数据时代的数据中台》', link: '/bigdata_notes/02.advance/D.《大数据时代的数据中台》-DataSir.html', target:'_blank' },
           { text: 'D.《大数据时代的知识图谱》', link: '/bigdata_notes/02.advance/D.《大数据时代的知识图谱》-Datasir.html', target:'_blank' },
           { text: 'D.《实用数据分析:数据分析师从小白到精通》', link: '/bigdata_notes/02.advance/D.《实用数据分析:数据分析师从小白到精通》.html', target:'_blank' },
@@ -31,12 +41,27 @@ module.exports = {
           { text: 'E.《数据中台实践课》', link: '/bigdata_notes/02.advance/E.《数据中台实践课》_郭忆_202003.html', target:'_blank' },
           { text: 'E.《Python数据分析与数据化运营》', link: '/bigdata_notes/02.advance/E.《Python数据分析与数据化运营》_宋天龙_201906.html', target:'_blank' },
           { text: 'E.《Python数据科学 - 技术详解与商业实践》', link: '/bigdata_notes/02.advance/E.《Python数据科学 - 技术详解与商业实践》_常国珍等_201811.html', target:'_blank' },
+          { text: 'E.《Python自动化测试实践》待整理', link: '/bigdata_notes/02.advance/404.html', target:'_blank' },
+          { text: 'E.《Selenium2/3 自动化测试实战，基于Python语言》待整理', link: '/bigdata_notes/02.advance/404.html', target:'_blank' },
+          { text: 'E.《App自动化测试与框架实战》待整理', link: '/bigdata_notes/02.advance/404.html', target:'_blank' },
+          { text: 'F.《Python金融大数据分析》待整理', link: '/bigdata_notes/02.advance/404.html', target:'_blank' },
+          { text: 'F.《量化投资以Python为工具》待整理', link: '/bigdata_notes/02.advance/404.html', target:'_blank' },
         ],
       },
       {
         text: "资深系列",
         items: [
           { text: 'G.《零基础Python自动化办公(漫画版)》', link: '/bigdata_notes/03.senior/G.《零基础Python自动化办公(漫画版)》.html', target:'_blank' },
+          { text: 'G.《推荐系统实践》待整理', link: '/bigdata_notes/03.senior/404.html', target:'_blank' },
+          { text: 'G.《推荐系统 技术、评估及高效算法（美）里奇编 2015》待整理', link: '/bigdata_notes/03.senior/404.html', target:'_blank' },
+          { text: 'H.《Python编程快速上手—让繁琐工作自动化》待整理', link: '/bigdata_notes/03.senior/404.html', target:'_blank' },
+          { text: 'H.《Python自动化运维-技术与最佳实践》待整理', link: '/bigdata_notes/03.senior/404.html', target:'_blank' },
+          { text: 'I.《Python+Spark 2.0+Hadoop机器学习与大数据实战_林大贵(著)》待整理', link: '/bigdata_notes/03.senior/404.html', target:'_blank' },
+          { text: 'I.《Python机器学习实践指南》待整理', link: '/bigdata_notes/03.senior/404.html', target:'_blank' },
+          { text: 'I.《艾伯特贝叶斯思维：统计建模的Python学习法》待整理', link: '/bigdata_notes/03.senior/404.html', target:'_blank' },
+          { text: 'I.《机器学习基础教程》待整理', link: '/bigdata_notes/03.senior/404.html', target:'_blank' },
+          { text: 'I.《集体智慧编程》待整理', link: '/bigdata_notes/03.senior/404.html', target:'_blank' },
+
         ]
       },
       // {
--- packages/bigdata_notes/docs/README.md
+++ packages/bigdata_notes/docs/README.md
@@ -7,13 +7,13 @@ actionText: 马上开始 →
 actionLink: /01.basis/
 features:
 - title: 基础系列
-  details: HTML5、CSS3、ES6、HTTP
+  details: Python、Django、Tornado、Flask
 
 - title: 进阶系列
-  details: 设计模式、异步编程、Vue&React、Koa&Mongo
+  details: 数据分析、数据挖掘、自动化测试、量化金融
 
 - title: 资深系列
-  details: 算法、微服务、DevOps、ServerLess
+  details: 推荐系统、自动化运维、机器学习、人工智能
 
 footer: MIT Licensed | Copyright © 2023-present 小磊哥er
 ---
