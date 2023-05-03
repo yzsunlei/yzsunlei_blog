ffe5f7afeeeba6bd4b8b9e311e170772f6ada094
sunlei <sunlei@jxgtmbjt.com>
2023-05-03 23:35:08 +0800

    feat: bigfed_nav子项目添加

---
+++ packages/bigfed_nav/docs/.vuepress/config.js
@@ -0,0 +1,18 @@
+module.exports = {
+  base: "/bigfed_nav/",
+  title: "小磊哥er的大前端学习导航",
+  themeConfig: {
+    nav: [
+      {
+        text: "首页",
+        link: "/",
+      },
+      {
+        text: "常用JavaScript库",
+        items: [
+          { text: '框架类库', link: '/01.packages/index.html#framework' },
+        ],
+      }
+    ],
+  },
+};
---
+++ packages/bigfed_nav/docs/README.md
@@ -0,0 +1,14 @@
+---
+home: true
+heroImage: /logo.jpg
+heroText: 大前端
+tagline: 让工作、生活充满节奏感
+actionText: 马上开始 →
+actionLink: /01.packages/
+features:
+- title: 框架类库
+  details: React、Vue、jQuery、RxJs
+
+
+footer: MIT Licensed | Copyright © 2023-present 小磊哥er
+---
