237eaf318868a57f0197bc78cee93d6984256956
sunlei <sunlei@jxgtmbjt.com>
2023-05-06 00:00:18 +0800

    feat: bigdata_automation子项目添加

---
+++ packages/bigdata_automation/docs/.vuepress/config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  base: "/bigdata_automation/",
+  title: "小磊哥er的大数智办公自动化专题",
+  themeConfig: {
+    nav: [
+      {
+        text: "总览",
+        link: "/",
+      },
+      {
+        text: "01.基础概述",
+        items: [
+          { text: '1.1.基本定义', link: '/bigfed_serverless/01.基础概述/1.1.基本定义.html', target:'_blank' },
+        ],
+      }
+    ],
+    search: false,
+    sidebar: 'auto',
+    sidebarDepth: 1
+  },
+};
