a18c32d467064f8b2899c705aa02aefe6e37fe0a
sunlei <sunlei@jxgtmbjt.com>
2023-05-13 12:39:24 +0800

    feat: automation子项目更新

--- packages/bigdata_automation/docs/.vuepress/config.js
+++ packages/bigdata_automation/docs/.vuepress/config.js
@@ -4,15 +4,43 @@ module.exports = {
   themeConfig: {
     nav: [
       {
-        text: "总览",
-        link: "/",
+        text: "01.文件目录操作自动化",
+        items: [
+          { text: '1.1.读写文件自动化', link: '/01.文件目录操作自动化/1.1.读写文件自动化.html'},
+          { text: '1.2.整理文件自动化', link: '/01.文件目录操作自动化/1.2.整理文件自动化.html'},
+        ],
       },
       {
-        text: "01.基础概述",
+        text: "02.Office办公自动化",
         items: [
-          { text: '1.1.基本定义', link: '/bigfed_serverless/01.基础概述/1.1.基本定义.html', target:'_blank' },
+          { text: '2.1.Excel表格自动化', link: '/02.Office办公自动化/2.1.Excel表格自动化.html'},
+          { text: '2.2.Word文档自动化', link: '/02.Office办公自动化/2.2.Word文档自动化.html'},
+          { text: '2.3.PPT演示自动化', link: '/02.Office办公自动化/2.3.PPT演示自动化.html'},
+          { text: '2.4.PDF文档自动化', link: '/02.Office办公自动化/2.4.PDF文档自动化.html'},
         ],
-      }
+      },
+      {
+        text: "03.多媒体处理自动化",
+        items: [
+          { text: '3.1.图像处理自动化', link: '/03.多媒体处理自动化/3.1.图像处理自动化.html'},
+          { text: '3.2.视频剪辑自动化', link: '/03.多媒体处理自动化/3.2.视频剪辑自动化.html'},
+          { text: '3.3.音频处理自动化', link: '/03.多媒体处理自动化/3.3.音频处理自动化.html'},
+        ],
+      },
+      {
+        text: "04.营销通知自动化",
+        items: [
+          { text: '4.1.邮件自动化', link: '/04.营销通知自动化/4.1.邮件自动化.html'},
+          { text: '4.2.短信自动化', link: '/04.营销通知自动化/4.2.短信自动化.html'},
+        ],
+      },
+      {
+        text: "05.软件操作自动化",
+        items: [
+          { text: '5.1.网站浏览器自动化', link: '/05.软件操作自动化/5.1.网站浏览器自动化.html'},
+          { text: '5.2.桌面软件自动化', link: '/05.软件操作自动化/5.2.桌面软件自动化.html'},
+        ],
+      },
     ],
     search: false,
     sidebar: 'auto',
--- packages/bigdata_automation/docs/README.md
+++ packages/bigdata_automation/docs/README.md
@@ -6,8 +6,16 @@ tagline: 让工作、生活充满节奏感
 actionText: 马上开始 →
 actionLink: /
 features:
-- title: 基础概述
-  details: 定义、现状、应用场景、业界产品
+- title: 文件目录操作
+  details: 读写文件、整理文件
+- title: Office办公处理
+  details: Excel处理、Word处理、PPT生成、PDF生成
+- title: 多媒体处理
+  details: 图像处理、视频剪辑、音频处理
+- title: 营销通知
+  details: 邮件营销、短信通知
+- title: 软件操作
+  details: 浏览器操作、桌面软件操作
 
 footer: MIT Licensed | Copyright © 2023-present 小磊哥er
 ---
